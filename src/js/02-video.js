3214074851e5e6292c5fc542997970debfac2551
Alexander Bohatskiy <obogatskiy@icloud.com>
2022-02-12 15:28:19 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+    const update = function(duration) {
+        console.log(duration.seconds);
+        localStorage.setItem('videoplayer-current-time', duration.seconds)
+    }
+    player.on('timeupdate', throttle(update, 1000 ));
+
+console.log(localStorage);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedData = JSON.parse(savedTime)
+console.log(parsedData);
+
+
+    
+player.setCurrentTime(parsedData).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
